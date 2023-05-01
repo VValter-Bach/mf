09135ea582838137e4c023e38b57ac779bc4aed9
VValter-Bach <valentin.assfalg@gmx.de>
2023-05-01 14:52:37 +0200

    added massive bullshit

--- main.c
+++ main.c
@@ -1,19 +1,59 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
+#include <stdlib.h>
+
+#include "rf95.h"
 
 #define XSTR(x) STR(x)
 #define STR(x) #x
+
+#define set_bit(reg,bit) reg |= (1<<bit)
+#define clr_bit(reg,bit) reg &= ~(1<<bit)
+#define check_bit(reg,bit) (reg&(1<<bit))
 
 #define STEPS_MIN 69
 #define STEPS_MAX 119
 #define STEPS_AVG 94
 #define COUNTER_MAX 188
+#define DATA_MAX 20
+
+#define FREQUENCY 434000000.0
+#define PREAMBLE 8
+// Pin definitions
+#define RST PB1
+#define SS PB2
+#define MOSI PB3
+#define MISO PB4
+#define SCK PB5
+
+#define LED1 PD3
+#define LED2 PD4
+#define LED3 PD5
+
+uint8_t state = 0;
+
+void led_setup(){
+	DDRD |= (1 << LED1) | (1 << LED2) | (1 << LED3);
+}
+
+void led_write(uint8_t l1, uint8_t l2, uint8_t l3){
+	if(l1) PORTD |= (1 << LED1);
+	else PORTD &= ~(1 << LED1);
+	if(l2) PORTD |= (1 << LED2);
+	else PORTD &= ~(1 << LED2);
+	if(l3) PORTD |= (1 << LED3);
+	else PORTD &= ~(1 << LED3);
+}
 
 // the counter of steps
 uint16_t counter = 0;
 // amount of steps needed to toggle PD6
 uint16_t steps = STEPS_AVG;
+
+uint8_t data[DATA_MAX];
+uint8_t len = 0;
+uint8_t pos = 0;
 
 void set_speed(uint8_t s)
 {
@@ -81,7 +121,7 @@ ISR(TIMER0_OVF_vect)
 	}
 }
 
-void setup_pwm()
+void pwm_setup()
 {
 	TCNT0 = 0;		// setting counter to 0
 	TCCR0B |= (1 << CS00);	// prescale 0
@@ -90,107 +130,163 @@ void setup_pwm()
 	sei();			// enabling global interrupts
 }
 
+/**
+ * @brief This function enables the spi bus
+ * @details It sets the SS, MOSI, SCK and RST pin to outputs
+ * enables SPI in Master mode and sets the sck rate to 1/16
+ */
+void spi_setup(){
+	DDRB |= (1 << SS) | (1 << MOSI) | (1 << SCK) | (1 << RST); // setting SS | MOSI | SCK to out
+	SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+	PORTB |= (1 << SS) | (1 << RST);
+}
+
+
+/**
+ * @brief Spi function for sending and recieving data simultanly 
+ * @param data The bite we send over the spi bus
+ * @return uint8_t The bite we recieve over the spi bus
+ */
+uint8_t spi_transcieve(uint8_t data){
+  SPDR = data;
+  while(!(SPSR & (1<<SPIF)));
+  return SPDR;
+}
+
+void spi_write_reg(uint8_t address, uint8_t value) {
+  PORTB &= ~(1 << SS); // Set SS to LOW
+  spi_transcieve(address | 0x80);   // Set write bit
+  spi_transcieve(value);
+  PORTB |= (1 << SS); // Set SS to HIGH
+}
+
+void spi_write_n(uint8_t address, uint8_t* data, uint8_t len){
+	PORTB &= ~(1 << SS); // Set SS to LOW
+	spi_transcieve(address | 0x80);
+	for(uint8_t i = 0; i < len; i++){
+		spi_transcieve(data[i]);
+	}
+	PORTB |= (1 << SS);
+}
+
+uint8_t spi_read_reg(uint8_t address) {
+  PORTB &= ~(1 << SS); // Set SS to LOW
+  spi_transcieve(address & 0x7F);   // Clear write bit
+  uint8_t value = spi_transcieve(0xFF); // Dummy Byte
+  PORTB |= (1 << SS); // Set SS to HIGH
+  return value;
+}
+
+void spi_read_n(uint8_t address, uint8_t* data, uint8_t len){
+	PORTB &= ~(1 << SS); // Set SS to LOW
+	spi_transcieve(address & 0x7F);
+	for(uint8_t i = 0; i < len; i++){
+		data[i] = spi_transcieve(0xFF); // sending dummy bytes
+	}
+	PORTB |= (1 << SS); // Set SS to HIGH
+}
+
+ISR(INT0_vect) {
+	spi_write_reg(RF95_12_IRQ_FLAGS, 0xFF);
+	state |= 0x80;
+}
+
+void rf95_setup(){
+	led_write(1,0,0);
+	// Enabling interrupt
+	//PCMSK0 |= (1 << PCINT0);  // Interrupt for Pin PCINT0
+	EIMSK |= (1 << INT0);    // set PCINT0-Interrupt-Flag
+	EICRA |= (1 << ISC01) | (1 << ISC00); // for rising
+
+	PORTB |= (1 << SS) | (1 << RST);
+	_delay_ms(10);
+	PORTB &= ~(1 << RST);
+	_delay_ms(10);
+	PORTB &= (1 << RST);
+	_delay_ms(10);
+	uint8_t bs = spi_read_reg(RF95_42_VERSION);
+	if(bs != 0x12){
+		while(1){
+			_delay_ms(100);
+		}
+	}
+	spi_write_reg(RF95_01_OP_MODE, RF95_LONG_RANGE_MODE);
+	_delay_ms(10);
+	//spi_write_reg(RF95_01_OP_MODE, RF95_MODE_STDBY);
+	//_delay_ms(10);
+	spi_write_reg(RF95_0E_FIFO_TX_BASE_ADDR, 0); // Setting TX data base index to 0
+	spi_write_reg(RF95_0F_FIFO_RX_BASE_ADDR, 0); // Setting RX data base index to 0
+	spi_write_reg(RF95_01_OP_MODE, RF95_MODE_STDBY);
+	spi_write_reg(RF95_40_DIO_MAPPING1, 0x40);
+	spi_write_reg(RF95_1D_MODEM_CONFIG1, 0x72); // Default bandwidth and Implicit Header mode
+	spi_write_reg(RF95_1E_MODEM_CONFIG2, 0x70); // TODO: revist
+	spi_write_reg(RF95_26_MODEM_CONFIG3, 0x04); // TODO: revist
+	uint32_t frf = FREQUENCY / RF95_FSTEP;
+	spi_write_reg(RF95_06_FRF_MSB, (frf >> 16) & 0xFF);
+	spi_write_reg(RF95_07_FRF_MID, (frf >> 8) & 0xFF);
+	spi_write_reg(RF95_08_FRF_LSB, frf & 0xFF);
+	spi_write_reg(RF95_20_PREAMBLE_MSB, (PREAMBLE >> 8) & 0xFF);
+	spi_write_reg(RF95_21_PREAMBLE_LSB, PREAMBLE & 0xFF);
+	spi_write_reg(RF95_09_PA_CONFIG, 0x80 | 11);
+	spi_write_reg(RF95_4D_PA_DAC, 0x04);
+	sei();
+	_delay_ms(1000);
+	led_write(0,0,0);
+}
+
+void rf95_send(uint8_t * data, uint8_t len){
+	state &= 0x7F;
+	spi_write_reg(RF95_01_OP_MODE, RF95_LONG_RANGE_MODE);
+	spi_write_reg(RF95_0E_FIFO_TX_BASE_ADDR, 0);
+	spi_write_n(RF95_00_FIFO, data, len);
+	spi_write_reg(RF95_22_PAYLOAD_LENGTH, len);
+	spi_write_reg(RF95_01_OP_MODE, RF95_MODE_TX);
+	spi_write_reg(RF95_40_DIO_MAPPING1, 0x40);
+//	spi_write_reg(RF95_01_OP_MODE, RF95_MODE_STDBY);
+}
+
+
 int main()
 {
-	setup_pwm();
-	DDRB = (1 << PB5);
-	_delay_ms(5000);
-	_delay_ms(5000);
-	PORTB |= (1 << PB5);
-	for (uint8_t i = 0; i < 100; i++) {
+	//clear_eeprom();
+	//pwm_setup();
+	led_setup();
+	led_write(0,0,0);
+	led_write(1,1,1);
+	spi_setup();
+	_delay_ms(1000);
+	led_write(1,1,0);
+	_delay_ms(1000);
+	rf95_setup();
+	_delay_ms(1000);
+	led_write(0,0,0);
+	uint8_t len = 19;
+	uint8_t* data = malloc(len);
+	for (int i = 0; i < len; i++){
+		data[i] = 0xFF;
+	}
+	_delay_ms(1000);
+	rf95_send(data, len); // TODO: anfangs problem
+	while(1){
+		if(state & 0x80) rf95_send(data, len);
+		_delay_ms(100);
+	}
+//	_delay_ms(5000);
+	//PORTB |= (1 << PB5);
+/*	for (uint8_t i = 0; i < 100; i++) {
 		set_speed(100 + i);
-		PORTB |= (1 << PB5);
+		//PORTB |= (1 << PB5);
 		_delay_ms(500);
-		PORTB &= ~(1 << PB5);
+		//PORTB &= ~(1 << PB5);
 		_delay_ms(500);
 	}
 
 	for (uint8_t i = 50;; i++) {
-		PORTB |= (1 << PB5);
+		//PORTB |= (1 << PB5);
 		_delay_ms(500);
-		PORTB &= ~(1 << PB5);
+		//PORTB &= ~(1 << PB5);
 		_delay_ms(500);
 
-	}
+	}*/
 }
 
-/*
-int main(){
-	setup_pwm();
-	DDRB |= (1<<PB5);
-	OCR0A = 0;
-	_delay_ms(5000);
-	OCR0A = MIDDLE;
-	_delay_ms(10);
-	OCR0A = 0;
-	_delay_ms(1000);
-	int8_t p = 1;
-	for(int8_t i = MIDDLE;;i+=p){
-		PORTB |= (1<<PB5);
-		_delay_ms(500);
-		PORTB &= ~(1<<PB5);
-		_delay_ms(500);
-		if (i == FORWARD) { p = -1; _delay_ms(3000);}
-		if (i == REVERSE) { p = 1; _delay_ms(3000);}
-	}
-}
-*/
-/*
-void clear_eeprom()
-{
-	for (uint16_t i = 0; i < 1024; i += 2) {
-		eeprom_update_word((uint16_t *) i, 0xFFFF);
-	}
-}
-
-// Main
-int main(void)
-{
-	DDRC = (1<<PC7); //setting LED to output
-	spi_init();		// Initialize SPI Master
-	_delay_ms(50);
-
-	PORTC = (1<<PC7);
-	PORTF = (1<<CS);
-	_delay_ms(2000);
-	PORTC &= ~(1<<PC7);
-	PORTF &= ~(1<<CS);
-	spi_tranceiver(0x01);	// Send "x", receive ACK in "data"
-	uint8_t reg = spi_tranceiver(0xFF);
-	PORTF |= (1<<CS);
-	eeprom_write_byte((uint8_t*)1, reg);
-	//reg = spi_tranceiver(0x7F);
-	//eeprom_write_byte((uint8_t*)2, reg);
-	//reg = spi_tranceiver(0x7F);
-	//eeprom_write_byte((uint8_t*)3, reg);
-	if(reg == 0x08 || reg == 0x09) PORTC |= (1<<PC7); // tunrning LED on
-	_delay_ms(3000);
-	while(1) {
-		_delay_ms(1000);
-	}
-}
-*/
-/*
- * int main() { clear_eeprom(); DDRC = (1<<PC7); //setting LED to output
- * PORTC = (1<<PC7); // tunrning LED on
- * 
- * setup_spi(); eeprom_write_byte ((uint8_t*) 0, SPCR); eeprom_write_byte
- * ((uint8_t*) 2, DDRB);
- * 
- * _delay_ms(1000);
- * 
- * wbr(PORTF, 0, PF1); // Setting to Low so radio turned on wbr(PORTC, 0,
- * PC7); // LED off _delay_ms(2000); // boot up time wbr(PORTF, 0, CS); // 
- * chip select Low transceive(0x01); transceive(0xAA); eeprom_write_byte
- * ((uint8_t*) 1, SPDR); transceive(0xAA); for(int i = 0; i < 1000; i++){
- * transceive(0xFF); } wbr(PORTF, 1, CS); _delay_ms(1000); wbr(PORTC, 1,
- * PC7); // LED on _delay_ms(500); //wbr(PORTC, 0, PC7); // LED off
- * _delay_ms(1000);
- * 
- * while(1){
- * 
- * int p = 1; for(char i = 46;;i = i + p){ OCR0A = i; _delay_ms(100);
- * //PORTC = (i%2<<PC7); if (i == 59) { p = -1; _delay_ms(3000); } if (i
- * == 31){ p = 1; _delay_ms(3000); } } } }
- * 
- */
---
+++ rf95.h
@@ -0,0 +1,218 @@
+// RH_RF95.h
+//
+// Definitions for HopeRF LoRa radios per:
+// http://www.hoperf.com/upload/rf/RFM95_96_97_98W.pdf
+// http://www.hoperf.cn/upload/rfchip/RF96_97_98.pdf
+//
+// Author: Mike McCauley (mikem@airspayce.com)
+// Copyright (C) 2014 Mike McCauley
+// $Id: RH_RF95.h,v 1.26 2020/06/15 23:39:39 mikem Exp $
+// 
+
+#ifndef RH_RF95_h
+#define RH_RF95_h
+
+// The crystal oscillator frequency of the module
+#define RF95_FXOSC 32000000.0
+
+// The Frequency Synthesizer step = RH_RF95_FXOSC / 2^^19
+#define RF95_FSTEP  (RF95_FXOSC / 524288)
+
+
+// Register names (LoRa Mode, from table 85)
+#define RF95_00_FIFO                                0x00
+#define RF95_01_OP_MODE                             0x01
+#define RF95_02_RESERVED                            0x02
+#define RF95_03_RESERVED                            0x03
+#define RF95_04_RESERVED                            0x04
+#define RF95_05_RESERVED                            0x05
+#define RF95_06_FRF_MSB                             0x06
+#define RF95_07_FRF_MID                             0x07
+#define RF95_08_FRF_LSB                             0x08
+#define RF95_09_PA_CONFIG                           0x09
+#define RF95_0A_PA_RAMP                             0x0a
+#define RF95_0B_OCP                                 0x0b
+#define RF95_0C_LNA                                 0x0c
+#define RF95_0D_FIFO_ADDR_PTR                       0x0d
+#define RF95_0E_FIFO_TX_BASE_ADDR                   0x0e
+#define RF95_0F_FIFO_RX_BASE_ADDR                   0x0f
+#define RF95_10_FIFO_RX_CURRENT_ADDR                0x10
+#define RF95_11_IRQ_FLAGS_MASK                      0x11
+#define RF95_12_IRQ_FLAGS                           0x12
+#define RF95_13_RX_NB_BYTES                         0x13
+#define RF95_14_RX_HEADER_CNT_VALUE_MSB             0x14
+#define RF95_15_RX_HEADER_CNT_VALUE_LSB             0x15
+#define RF95_16_RX_PACKET_CNT_VALUE_MSB             0x16
+#define RF95_17_RX_PACKET_CNT_VALUE_LSB             0x17
+#define RF95_18_MODEM_STAT                          0x18
+#define RF95_19_PKT_SNR_VALUE                       0x19
+#define RF95_1A_PKT_RSSI_VALUE                      0x1a
+#define RF95_1B_RSSI_VALUE                          0x1b
+#define RF95_1C_HOP_CHANNEL                         0x1c
+#define RF95_1D_MODEM_CONFIG1                       0x1d
+#define RF95_1E_MODEM_CONFIG2                       0x1e
+#define RF95_1F_SYMB_TIMEOUT_LSB                    0x1f
+#define RF95_20_PREAMBLE_MSB                        0x20
+#define RF95_21_PREAMBLE_LSB                        0x21
+#define RF95_22_PAYLOAD_LENGTH                      0x22
+#define RF95_23_MAX_PAYLOAD_LENGTH                  0x23
+#define RF95_24_HOP_PERIOD                          0x24
+#define RF95_25_FIFO_RX_BYTE_ADDR                   0x25
+#define RF95_26_MODEM_CONFIG3                       0x26
+#define RF95_27_PPM_CORRECTION                      0x27
+#define RF95_28_FEI_MSB                             0x28
+#define RF95_29_FEI_MID                             0x29
+#define RF95_2A_FEI_LSB                             0x2a
+#define RF95_2C_RSSI_WIDEBAND                       0x2c
+#define RF95_31_DETECT_OPTIMIZE                     0x31
+#define RF95_33_INVERT_IQ                           0x33
+#define RF95_37_DETECTION_THRESHOLD                 0x37
+#define RF95_39_SYNC_WORD                           0x39
+#define RF95_40_DIO_MAPPING1                        0x40
+#define RF95_41_DIO_MAPPING2                        0x41
+#define RF95_42_VERSION                             0x42
+#define RF95_4B_TCXO                                0x4b
+#define RF95_4D_PA_DAC                              0x4d
+#define RF95_5B_FORMER_TEMP                         0x5b
+#define RF95_61_AGC_REF                             0x61
+#define RF95_62_AGC_THRESH1                         0x62
+#define RF95_63_AGC_THRESH2                         0x63
+#define RF95_64_AGC_THRESH3                         0x64
+
+// RH_RF95_REG_01_OP_MODE                             0x01
+#define RF95_LONG_RANGE_MODE                       0x80
+#define RF95_ACCESS_SHARED_REG                     0x40
+#define RF95_LOW_FREQUENCY_MODE                    0x08
+#define RF95_MODE                                  0x07
+#define RF95_MODE_SLEEP                            0x00
+#define RF95_MODE_STDBY                            0x01
+#define RF95_MODE_FSTX                             0x02
+#define RF95_MODE_TX                               0x03
+#define RF95_MODE_FSRX                             0x04
+#define RF95_MODE_RXCONTINUOUS                     0x05
+#define RF95_MODE_RXSINGLE                         0x06
+#define RF95_MODE_CAD                              0x07
+
+// RH_RF95_REG_09_PA_CONFIG                           0x09
+#define RF95_PA_SELECT                             0x80
+#define RF95_MAX_POWER                             0x70
+#define RF95_OUTPUT_POWER                          0x0f
+
+// RH_RF95_REG_0A_PA_RAMP                             0x0a
+#define RF95_LOW_PN_TX_PLL_OFF                     0x10
+#define RF95_PA_RAMP                               0x0f
+#define RF95_PA_RAMP_3_4MS                         0x00
+#define RF95_PA_RAMP_2MS                           0x01
+#define RF95_PA_RAMP_1MS                           0x02
+#define RF95_PA_RAMP_500US                         0x03
+#define RF95_PA_RAMP_250US                         0x04
+#define RF95_PA_RAMP_125US                         0x05
+#define RF95_PA_RAMP_100US                         0x06
+#define RF95_PA_RAMP_62US                          0x07
+#define RF95_PA_RAMP_50US                          0x08
+#define RF95_PA_RAMP_40US                          0x09
+#define RF95_PA_RAMP_31US                          0x0a
+#define RF95_PA_RAMP_25US                          0x0b
+#define RF95_PA_RAMP_20US                          0x0c
+#define RF95_PA_RAMP_15US                          0x0d
+#define RF95_PA_RAMP_12US                          0x0e
+#define RF95_PA_RAMP_10US                          0x0f
+
+// RH_RF95_REG_0B_OCP                                 0x0b
+#define RF95_OCP_ON                                0x20
+#define RF95_OCP_TRIM                              0x1f
+
+// RH_RF95_REG_0C_LNA                                 0x0c
+#define RF95_LNA_GAIN                              0xe0
+#define RF95_LNA_GAIN_G1                           0x20
+#define RF95_LNA_GAIN_G2                           0x40
+#define RF95_LNA_GAIN_G3                           0x60                
+#define RF95_LNA_GAIN_G4                           0x80
+#define RF95_LNA_GAIN_G5                           0xa0
+#define RF95_LNA_GAIN_G6                           0xc0
+#define RF95_LNA_BOOST_LF                          0x18
+#define RF95_LNA_BOOST_LF_DEFAULT                  0x00
+#define RF95_LNA_BOOST_HF                          0x03
+#define RF95_LNA_BOOST_HF_DEFAULT                  0x00
+#define RF95_LNA_BOOST_HF_150PC                    0x03
+
+// RH_RF95_REG_11_IRQ_FLAGS_MASK                      0x11
+#define RF95_RX_TIMEOUT_MASK                       0x80
+#define RF95_RX_DONE_MASK                          0x40
+#define RF95_PAYLOAD_CRC_ERROR_MASK                0x20
+#define RF95_VALID_HEADER_MASK                     0x10
+#define RF95_TX_DONE_MASK                          0x08
+#define RF95_CAD_DONE_MASK                         0x04
+#define RF95_FHSS_CHANGE_CHANNEL_MASK              0x02
+#define RF95_CAD_DETECTED_MASK                     0x01
+
+// RH_RF95_REG_12_IRQ_FLAGS                           0x12
+#define RF95_RX_TIMEOUT                            0x80
+#define RF95_RX_DONE                               0x40
+#define RF95_PAYLOAD_CRC_ERROR                     0x20
+#define RF95_VALID_HEADER                          0x10
+#define RF95_TX_DONE                               0x08
+#define RF95_CAD_DONE                              0x04
+#define RF95_FHSS_CHANGE_CHANNEL                   0x02
+#define RF95_CAD_DETECTED                          0x01
+
+// RH_RF95_REG_18_MODEM_STAT                          0x18
+#define RF95_RX_CODING_RATE                        0xe0
+#define RF95_MODEM_STATUS_CLEAR                    0x10
+#define RF95_MODEM_STATUS_HEADER_INFO_VALID        0x08
+#define RF95_MODEM_STATUS_RX_ONGOING               0x04
+#define RF95_MODEM_STATUS_SIGNAL_SYNCHRONIZED      0x02
+#define RF95_MODEM_STATUS_SIGNAL_DETECTED          0x01
+
+// RH_RF95_REG_1C_HOP_CHANNEL                         0x1c
+#define RF95_PLL_TIMEOUT                           0x80
+#define RF95_RX_PAYLOAD_CRC_IS_ON                  0x40
+#define RF95_FHSS_PRESENT_CHANNEL                  0x3f
+
+// RH_RF95_REG_1D_MODEM_CONFIG1                       0x1d
+#define RF95_BW                                    0xf0
+
+#define RF95_BW_7_8KHZ                             0x00
+#define RF95_BW_10_4KHZ                            0x10
+#define RF95_BW_15_6KHZ                            0x20
+#define RF95_BW_20_8KHZ                            0x30
+#define RF95_BW_31_25KHZ                           0x40
+#define RF95_BW_41_7KHZ                            0x50
+#define RF95_BW_62_5KHZ                            0x60
+#define RF95_BW_125KHZ                             0x70
+#define RF95_BW_250KHZ                             0x80
+#define RF95_BW_500KHZ                             0x90
+#define RF95_CODING_RATE                           0x0e
+#define RF95_CODING_RATE_4_5                       0x02
+#define RF95_CODING_RATE_4_6                       0x04
+#define RF95_CODING_RATE_4_7                       0x06
+#define RF95_CODING_RATE_4_8                       0x08
+#define RF95_IMPLICIT_HEADER_MODE_ON               0x01
+
+// RH_RF95_REG_1E_MODEM_CONFIG2                       0x1e
+#define RF95_SPREADING_FACTOR                      0xf0
+#define RF95_SPREADING_FACTOR_64CPS                0x60
+#define RF95_SPREADING_FACTOR_128CPS               0x70
+#define RF95_SPREADING_FACTOR_256CPS               0x80
+#define RF95_SPREADING_FACTOR_512CPS               0x90
+#define RF95_SPREADING_FACTOR_1024CPS              0xa0
+#define RF95_SPREADING_FACTOR_2048CPS              0xb0
+#define RF95_SPREADING_FACTOR_4096CPS              0xc0
+#define RF95_TX_CONTINUOUS_MODE                    0x08
+
+#define RF95_PAYLOAD_CRC_ON                        0x04
+#define RF95_SYM_TIMEOUT_MSB                       0x03
+
+// RH_RF95_REG_26_MODEM_CONFIG3
+#define RF95_MOBILE_NODE                           0x08 // HopeRF term
+#define RF95_LOW_DATA_RATE_OPTIMIZE                0x08 // Semtechs term
+#define RF95_AGC_AUTO_ON                           0x04
+
+// RH_RF95_REG_4B_TCXO                                0x4b
+#define RF95_TCXO_TCXO_INPUT_ON                    0x10
+
+// RH_RF95_REG_4D_PA_DAC                              0x4d
+#define RF95_PA_DAC_DISABLE                        0x04
+#define RF95_PA_DAC_ENABLE                         0x07
+
+#endif
